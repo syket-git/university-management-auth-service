f3d27f67c718aef7f6bb2748315efe501c455619
syket <syketweb@gmail.com>
2023-12-12 11:15:55 +0600

    husky test

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,18 @@
-import cors from "cors";
-import express, { Application, Request, Response } from "express";
+import cors from 'cors'
+import express, { Application, Request, Response } from 'express'
 
-const app: Application = express();
+const app: Application = express()
 
-app.use(cors());
+app.use(cors())
 
 // parser
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
+
+app.use(express.urlencoded({ extended: true }))
 
 // Testing route
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
-});
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!')
+})
 
-export default app;
+export default app
