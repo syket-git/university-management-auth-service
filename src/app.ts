938976f5bc24e3917e59cd87fad3d78448490ceb
syket <syketweb@gmail.com>
2023-12-12 11:48:57 +0600

    setup husky and lint-staged

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import cors from 'cors'
-import express, { Application, Request } from 'express'
+import express, { Application, Request, Response } from 'express'
 
 const app: Application = express()
 
