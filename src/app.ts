0ec53e60f93d54dc082d9357a7c175d46c04e6f1
syket <syketweb@gmail.com>
2023-12-12 12:22:58 +0600

    test

--- src/app.ts
+++ src/app.ts
@@ -11,7 +11,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 // Testing route
-app.get('/', (req: any, res: any) => {
+app.get('/', (req: any, res) => {
   res.send('Hello World!')
 })
 
