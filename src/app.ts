7dce2de6721f4176543c2cab7f8571af15037dda
syket <syketweb@gmail.com>
2023-12-16 23:46:18 +0600

    globalErrorHandler function added

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import cors from 'cors'
 import express, { Application, Request, Response } from 'express'
 
+import globalErrorHandler from './app/middlewares/globalErrorHandler'
 import usersRouter from './app/modules/users/users.route'
 
 const app: Application = express()
@@ -18,6 +19,10 @@ app.use('/api/v1/users', usersRouter)
 // Testing route
 app.get('/', async (req: Request, res: Response) => {
   res.send('Hello World!')
+  // throw new ApiError(400, 'New Error generated')
 })
+
+//Global error handler
+app.use(globalErrorHandler)
 
 export default app
