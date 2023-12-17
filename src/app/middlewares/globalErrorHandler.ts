460ed004ca3d18ac11dbb7dad4a7aab1bce4ddba
syket <syketweb@gmail.com>
2023-12-17 18:54:10 +0600

    working on the globalErrorHandler function

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -1,12 +1,40 @@
 import { NextFunction, Request, Response } from 'express'
+import mongoose from 'mongoose'
+import config from '../../config'
+import ApiError from '../../error/ApiError'
+import handleValidationError from '../../error/handleValidationError'
+import { IGenericErrorMessage } from '../../interfaces/error'
 
 const globalErrorHandler = (
-  err: Error,
+  err: mongoose.Error.ValidationError,
   req: Request,
   res: Response,
   next: NextFunction,
 ) => {
-  res.status(400).json({ errors: err })
+  let statusCode = 500
+  let message = 'Something went wrong'
+  let errorMessages: IGenericErrorMessage[] = []
+
+  if (err?.name === 'ValidationError') {
+    const simplifiedError = handleValidationError(err)
+    statusCode = simplifiedError.statusCode
+    message = simplifiedError.message
+    errorMessages = simplifiedError.errorMessage
+  } else if (err instanceof ApiError) {
+    statusCode = err?.statusCode
+    message = err?.message
+    errorMessages = err?.message ? [{ path: '', message: err?.message }] : []
+  } else if (err instanceof Error) {
+    message = err?.message
+    errorMessages = err?.message ? [{ path: '', message: err?.message }] : []
+  }
+  res.status(statusCode).json({
+    success: false,
+    message,
+    errorMessages,
+    stack: config.env === 'development' ? err.stack : undefined,
+  })
+
   next()
 }
 
---
+++ src/error/handleValidationError.ts
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose'
+import { IGenericErrorResponse } from '../interfaces/common'
+import { IGenericErrorMessage } from '../interfaces/error'
+
+const handleValidationError = (
+  err: mongoose.Error.ValidationError | null | undefined,
+): IGenericErrorResponse => {
+  const errors: IGenericErrorMessage[] = Object.values(err?.errors || {}).map(
+    e => {
+      return {
+        path: e?.path || '',
+        message: e?.message || '',
+      }
+    },
+  )
+
+  const statusCode = 400
+  return {
+    statusCode,
+    message: 'Validation Error',
+    errorMessage: errors,
+  }
+}
+
+export default handleValidationError
