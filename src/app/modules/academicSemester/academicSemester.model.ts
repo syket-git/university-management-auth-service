d5000c5ebd86cde6ebcc75f7ce96cdb4e50f2eb7
syket <syketweb@gmail.com>
2023-12-21 12:02:37 +0600

    created academicSemester model and schema

---
+++ src/app/modules/academicSemester/academicSemester.model.ts
@@ -0,0 +1,26 @@
+import { Schema, model } from 'mongoose';
+import {
+  IAcademicSemester,
+  IAcademicSemesterModel,
+} from './academicSemester.interface';
+
+const academicSemesterSchema = new Schema<IAcademicSemester>(
+  {
+    title: { type: String, required: true },
+    year: {
+      type: Number,
+      required: true,
+    },
+    code: { type: String, required: true },
+    startMonth: { type: String, required: true },
+    endMonth: { type: String, required: true },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export const AcademicSemester = model<
+  IAcademicSemester,
+  IAcademicSemesterModel
+>('AcademicSemester', academicSemesterSchema);
