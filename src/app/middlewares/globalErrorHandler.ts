84ee0417d006e8fbabb041f478f71d50df5fb349
syket <syketweb@gmail.com>
2023-12-21 11:46:00 +0600

    zodError handling completed

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -1,43 +1,51 @@
+import { IGenericErrorMessage } from './../../interfaces/error';
 /* eslint-disable no-console */
 /* eslint-disable no-unused-expressions */
 
-import { ErrorRequestHandler } from 'express'
-import config from '../../config'
-import ApiError from '../../error/ApiError'
-import handleValidationError from '../../error/handleValidationError'
-import { IGenericErrorMessage } from '../../interfaces/error'
-import { errorLogger } from '../../shared/logger'
+import { ErrorRequestHandler } from 'express';
+import { ZodError } from 'zod';
+import config from '../../config';
+import ApiError from '../../error/ApiError';
+import handleValidationError from '../../error/handleValidationError';
+import handleZodError from '../../error/handleZodError';
+import { errorLogger } from '../../shared/logger';
 
 const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
   config.env === 'development'
     ? console.log('globalErrorHandler ~ ', err)
-    : errorLogger.error(err)
+    : errorLogger.error(err);
 
-  let statusCode = 500
-  let message = 'Something went wrong'
-  let errorMessages: IGenericErrorMessage[] = []
+  let statusCode = 500;
+  let message = 'Something went wrong';
+  let errorMessages: IGenericErrorMessage[] = [];
 
   if (err?.name === 'ValidationError') {
-    const simplifiedError = handleValidationError(err)
-    statusCode = simplifiedError.statusCode
-    message = simplifiedError.message
-    errorMessages = simplifiedError.errorMessage
+    const simplifiedError = handleValidationError(err);
+    statusCode = simplifiedError.statusCode;
+    message = simplifiedError.message;
+    errorMessages = simplifiedError.errorMessage;
+  } else if (err instanceof ZodError) {
+    const simplifiedError = handleZodError(err);
+
+    statusCode = simplifiedError.statusCode;
+    message = simplifiedError.message;
+    errorMessages = simplifiedError.errorMessage;
   } else if (err instanceof ApiError) {
-    statusCode = err?.statusCode
-    message = err?.message
-    errorMessages = err?.message ? [{ path: '', message: err?.message }] : []
+    statusCode = err?.statusCode;
+    message = err?.message;
+    errorMessages = err?.message ? [{ path: '', message: err?.message }] : [];
   } else if (err instanceof Error) {
-    message = err?.message
-    errorMessages = err?.message ? [{ path: '', message: err?.message }] : []
+    message = err?.message;
+    errorMessages = err?.message ? [{ path: '', message: err?.message }] : [];
   }
   res.status(statusCode).json({
     success: false,
     message,
     errorMessages,
     stack: config.env === 'development' ? err.stack : undefined,
-  })
+  });
 
-  next()
-}
+  next();
+};
 
-export default globalErrorHandler
+export default globalErrorHandler;
---
+++ src/error/handleZodError.ts
@@ -0,0 +1,20 @@
+import { ZodError, ZodIssue } from 'zod';
+import { IGenericErrorResponse } from '../interfaces/common';
+import { IGenericErrorMessage } from '../interfaces/error';
+
+const handleZodError = (err: ZodError): IGenericErrorResponse => {
+  const errors: IGenericErrorMessage[] = err.issues.map((issue: ZodIssue) => {
+    return {
+      path: issue?.path[issue.path.length - 1],
+      message: issue?.message,
+    };
+  });
+
+  return {
+    statusCode: 400,
+    message: 'Validation Error',
+    errorMessage: errors,
+  };
+};
+
+export default handleZodError;
