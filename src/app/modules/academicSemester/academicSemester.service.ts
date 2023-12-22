bf3d2aa4d5a8b4fe05c8fb507c4d56f50088feea
syket <syketweb@gmail.com>
2023-12-22 10:55:08 +0600

    academic semester code validation added

--- src/app/modules/academicSemester/academicSemester.service.ts
+++ src/app/modules/academicSemester/academicSemester.service.ts
@@ -1,9 +1,16 @@
+import status from 'http-status';
+import ApiError from '../../../error/ApiError';
+import { academicSemesterTitleCodeMapper } from './academicSemester.constant';
 import { IAcademicSemester } from './academicSemester.interface';
 import { AcademicSemester } from './academicSemester.model';
 
 const createSemester = async (
   payload: IAcademicSemester,
 ): Promise<IAcademicSemester> => {
+  if (academicSemesterTitleCodeMapper[payload.title] !== payload.code) {
+    throw new ApiError(status.BAD_REQUEST, 'Invalid semester code');
+  }
+
   const result = await AcademicSemester.create(payload);
   return result;
 };
